ab359ee8c231d2657a848be1dff6c78bec86cc14
Tyler Crawford <tcrawford@bmss.com>
2025-07-21 10:02:08 -0500

    fix directory prob

--- client/src/pages/ExistingDrafts.js
+++ client/src/pages/ExistingDrafts.js
@@ -1,8 +1,8 @@
 import { useEffect, useState, useMemo } from 'react';
-import Sidebar from '../../components/Sidebar';
-import KpiShell from '../../components/KPIShell';
-import GeneralDataTable from '../../components/DataTable';
-import TopBar from '../../components/TopBar';
+import Sidebar from '../components/Sidebar';
+import KpiShell from '../components/KPIShell';
+import GeneralDataTable from '../components/DataTable';
+import TopBar from '../components/TopBar';
 
 
 export default function ExistingDrafts() {
