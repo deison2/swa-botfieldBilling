2c12ea57a402924f55852a4af878b7706ce36dc2
Tyler Crawford <tcrawford@bmss.com>
2025-08-04 16:40:54 -0500

    Update loader formatting

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -4,9 +4,16 @@ import './Loader.css';
 export default function Loader() {
   return (
     <div className="global-loader-overlay">
-      <div className="push-pop loader">
-        <div></div>
-        <div></div>
+      {/* two matching animations side-by-side */}
+      <div className="loader-boxes">
+        <div className="push-pop loader">
+          <div></div>
+          <div></div>
+        </div>
+        <div className="push-out loader">
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   );
